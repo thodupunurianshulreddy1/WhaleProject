85e01cf9c9d10276f0607938f225b2ac562c35e9
Jeannette Garea Courtois <jeannette.garea.c@gmail.com>
2026-06-14 16:22:38 +0200

    ok

--- airflow/whale_wallet_pipeline.py
+++ airflow/whale_wallet_pipeline.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from airflow import DAG
-from airflow.operators.empty import EmptyOperator
+#from airflow.operators.empty import EmptyOperator
 
 with DAG(
     dag_id="whale_wallet_tracking_pipeline",
